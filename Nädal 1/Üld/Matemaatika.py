a9bf1e2b5be7d09d89555a4c2de56bc22c38db74
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-02-11 13:46:36 +0200

    Klassi ja eksempläri loomis katse

---
+++ Nädal 1/Üld/Matemaatika.py
@@ -0,0 +1,4 @@
+class Matemaatika():
+    number = 5
+    def liitmine(self, arv):
+        print(Matemaatika.number + arv)
---
+++ Nädal 1/Üld/test.py
@@ -0,0 +1,8 @@
+from Matemaatika import Matemaatika
+#from file import class
+a = Matemaatika()
+print(a)
+print(a.number)
+a.number = 8
+print(a.number)
+a.liitmine(4)
