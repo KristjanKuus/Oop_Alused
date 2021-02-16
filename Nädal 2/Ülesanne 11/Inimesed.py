a9a6a11bd1daef063e2a004c6598a5204ff1982a
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-02-16 11:44:05 +0200

    Ülesanne 11

---
+++ Nädal 2/Ülesanne 11/Inimesed.py
@@ -0,0 +1,35 @@
+from random import randint
+idlugeja = 0
+armee1 = []
+armee2 = []
+
+class inimene:
+    def __init__(self):
+        self.id = 0
+
+    def id_jagaja(self):
+        global idlugeja
+        while idlugeja == 0:
+           if self.id == 0:
+            self.id = 1
+            idlugeja = idlugeja + 1
+        while idlugeja >= 1:
+            idlugeja = idlugeja + 1
+            self.id = idlugeja - 1
+            break
+
+    def info(self):
+        print("Inimse ID on:", self.id)
+
+class sodur(inimene):
+    def armee(self):
+        self.armee = randint(1,2)
+
+    def printer(self):
+        print(self.id, self.armee)
+
+    def armeeleidja(self):
+        if self.armee == 1:
+            armee1.append(str(self))
+        if self.armee == 2:
+            armee2.append(str(self))
