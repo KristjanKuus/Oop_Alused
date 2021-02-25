921dacc66e35e589a3c24c88c397537b1f658f1e
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-02-25 10:39:17 +0200

    Ülesanne 16

---
+++ Nädal 3/Ülesanne 16/Ülesanne16.py
@@ -0,0 +1,10 @@
+from Kasutaja import *
+
+Kasutaja1 = Kasutaja("Jass", "Juss", "Kasutaja")
+Kasutaja2 = Kasutaja("Tege", "Lane","Kasutaja")
+Kasutaja3 = Kasutaja("Jooma", "Mees","Kasutaja")
+
+Admin1 = Admin("Teet", "Peet", "Administraator")
+
+Admin1.kirjelda_kasutaja()
+Admin1.naita_privileegid()
